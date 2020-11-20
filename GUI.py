dfdce2b1e652e970f0814d0a1754a73c2c4592c6
Lzimmer7 <69931110+Lzimmer7@users.noreply.github.com>
2020-11-20 17:29:21 -0500

    Update GUI.py
    Changed date, time and calculate button font color from white to black

--- GUI.py
+++ GUI.py
@@ -99,7 +99,7 @@ frame.place(relwidth = 1, relheight = 1)
 
 # -----Import Time------
 timefont = Font(family = "Open Sans", size = 14)
-time_label = tk.Label(root, font = timefont, bg ='#0080ff', fg = '#f2f2f2')
+time_label = tk.Label(root, font = timefont, bg ='#0080ff', fg = '#000000')
 time_label.place(relx = 0.01, rely = 0.02)
 update_time()
 
@@ -234,7 +234,7 @@ label_to.place(relx = 0.497, rely = 0.7055)
 # -----Calculate Button-----#
 calc_font = Font(family = "Open Sans", size = 16, slant = "italic")
 calc_button = tk.Button(frame, text = "Calculate", font = calc_font, bg = '#0080ff',
-		fg = '#f2f2f2', activebackground = '#005c99', activeforeground = '#f2f2f2', 
+		fg = '#000000', activebackground = '#005c99', activeforeground = '#f2f2f2', 
 		justify = "center", highlightthickness = 0, bd = 1, relief = "raised",
 		command = lambda: f.get_conversion(amount.get(), combo_box1.get(), combo_box2.get(), entryText), 
 		padx = 1, pady = 1)
